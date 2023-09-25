24a192bc329560a696871fae0c5351488120f942
Noah Bargy_Du_Gardin <noah.bargydugardin@epfedu.fr>
2023-09-25 19:18:21 +0200

    Commit fin du tp0 calculator

--- TP0_BARGY DU GARDIN NOAH/Calculator/Calculator/src/calculator/Calculator.java
+++ TP0_BARGY DU GARDIN NOAH/Calculator/Calculator/src/calculator/Calculator.java
@@ -15,23 +15,50 @@ public class Calculator {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-         
+public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-                System.out.println("Please enter the operator:");
+        System.out.println("1) Addition");
+        System.out.println("2) Soustraction");
+        System.out.println("3) Multiplication");
+        System.out.println("4) Division");
+        System.out.println("5) Modulo");
+        System.out.println("Veuillez entrer l'operation souhaitee :");
+
         int operateur = scanner.nextInt();
-        scanner.close();
-        System.out.println("1) add");
-        System.out.println("2) subtract");
-        System.out.println("3) multiply");
-        System.out.println("4) divide");
-        System.out.println("5) modulo");
-        System.out.print("Veuillez entrer la première valeur (opérande1) : ");
-        double operande1 = scanner.nextDouble();
-        scanner = new Scanner(System.in);
-        System.out.print("Veuillez entrer la première valeur (opérande1) : ");
-        double operande2 = scanner.nextDouble();
+        if (operateur < 1 || operateur > 5) {
+            System.out.println("Operateur non valide.");
+            scanner.close();
+            return; // Sort du programme si l'opérateur n'est pas valide.
+        }
         
-    }
-    
-}
+        System.out.print("Veuillez entrer la première valeur (operande1) : ");
+        double operande1 = scanner.nextDouble();
+
+        System.out.print("Veuillez entrer la deuxième valeur (operande2) : ");
+        double operande2 = scanner.nextDouble();
+        scanner.close();
+        double resultat = 0.0;
+         switch (operateur) {//
+            case 1:
+                resultat = operande1 + operande2;
+                break;
+            case 2:
+                resultat = operande1 - operande2;
+                break;
+            case 3:
+                resultat = operande1 * operande2;
+                break;
+            case 4:
+                if (operande2 != 0) {
+                    resultat = operande1 / operande2;
+                } else {
+                    System.out.println("Erreur : Division par zero.");
+                    return; // Sort du programme si la division par zéro est détectée.
+                }
+                break;
+            case 5:
+                resultat = operande1 % operande2;
+                break;      
+        }
+          System.out.println("Le résultat est : " + resultat);
+}}
