95294df431f21ad179a2503c42730c1ae55cde35
Noah Bargy_Du_Gardin <noah.bargydugardin@epfedu.fr>
2023-10-06 15:26:18 +0200

    Fin du tp1

---
+++ TP2_Bieres_BARGY_DU_GARDIN/src/tp2_bieres_bargy_du_gardin/TP2_Bieres_BARGY_DU_GARDIN.java
@@ -0,0 +1,20 @@
+    /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_bieres_bargy_du_gardin;
+
+/**
+ *
+ * @author lebgd
+ */
+public class TP2_Bieres_BARGY_DU_GARDIN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
