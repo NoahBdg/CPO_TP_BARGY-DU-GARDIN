d9f104a8bbb456dea2b57bb0d01c21da77b899ca
Noah Bargy_Du_Gardin <noah.bargydugardin@epfedu.fr>
2023-10-08 22:14:11 +0200

    Fin exo2 DU TP2

---
+++ TP2_convertisseurObjet_BARGY/src/tp2_convertisseurobjet_bargy/TP2_convertisseurObjet_BARGY.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_convertisseurobjet_bargy;
+
+/**
+ *
+ * @author lebgd
+ */
+public class TP2_convertisseurObjet_BARGY {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Convertisseur cirl= new Convertisseur();
+        
+    }
+    
+}
