564ace42c1854fa9b1b72945aed207b2badaa255
Noah Bargy_Du_Gardin <noah.bargydugardin@epfedu.fr>
2023-10-27 12:34:45 +0200

    Classe GrilleDeJeu fonctionnelle

---
+++ LightOff_BARGY_DU_GARDIN_version_console/src/lightoff_bargy_du_gardin_version_console/GrilleDeJeu.java
@@ -0,0 +1,159 @@
+package lightoff_bargy_du_gardin_version_console;
+
+import java.util.Random;
+
+import java.util.Random;
+
+public class GrilleDeJeu {
+    private CelluleLumineuse[][] matriceCellules;
+    private int nbLignes;
+    private int nbColonnes;
+
+    // Constructeur de la classe
+    public GrilleDeJeu(int p_nbLignes, int p_nbColonnes) {
+        nbLignes = p_nbLignes;
+        nbColonnes = p_nbColonnes;
+        matriceCellules = new CelluleLumineuse[nbLignes][nbColonnes];
+
+        // Initialisation de la matrice avec des cellules lumineuses éteintes
+        for (int i = 0; i < nbLignes; i++) {
+            for (int j = 0; j < nbColonnes; j++) {
+                matriceCellules[i][j] = new CelluleLumineuse();
+            }
+        }
+    }
+
+    // Méthode pour éteindre toutes les cellules de la grille
+    public void eteindreToutesLesCellules() {
+        for (int i = 0; i < nbLignes; i++) {
+            for (int j = 0; j < nbColonnes; j++) {
+                matriceCellules[i][j].eteindreCellule();
+            }
+        }
+    }
+
+    // Méthode pour activer de manière aléatoire une ligne, une colonne ou une diagonale
+    public void activerLigneColonneOuDiagonaleAleatoire() {
+        Random random = new Random();
+        int choix = random.nextInt(3); // 0 pour ligne, 1 pour colonne, 2 pour diagonale
+
+        switch (choix) {
+            case 0:
+                int ligne = random.nextInt(nbLignes);
+                for (int j = 0; j < nbColonnes; j++) {
+                    matriceCellules[ligne][j].activerCellule();
+                }
+                break;
+            case 1:
+                int colonne = random.nextInt(nbColonnes);
+                for (int i = 0; i < nbLignes; i++) {
+                    matriceCellules[i][colonne].activerCellule();
+                }
+                break;
+            case 2:
+                // Activation de la diagonale descendante
+                for (int i = 0; i < nbLignes; i++) {
+                    matriceCellules[i][i].activerCellule();
+                }
+                // Activation de la diagonale montante
+                for (int i = 0; i < nbLignes; i++) {
+                    matriceCellules[i][nbColonnes - i - 1].activerCellule();
+                }
+                break;
+        }
+    }
+
+    // Méthode pour mélanger la matrice de cellules aléatoirement
+    public void melangerMatriceAleatoirement(int nbTours) {
+        eteindreToutesLesCellules();
+        Random random = new Random();
+        for (int tour = 0; tour < nbTours; tour++) {
+            activerLigneColonneOuDiagonaleAleatoire();
+        }
+    }
+
+    // Méthode pour activer une ligne spécifique de cellules
+    public void activerLigneDeCellules(int idLigne) {
+        for (int j = 0; j < nbColonnes; j++) {
+            matriceCellules[idLigne][j].activerCellule();
+        }
+    }
+
+    // Méthode pour activer une colonne spécifique de cellules
+    public void activerColonneDeCellules(int idColonne) {
+        for (int i = 0; i < nbLignes; i++) {
+            matriceCellules[i][idColonne].activerCellule();
+        }
+    }
+
+    // Méthodes pour activer les diagonales
+    public void activerDiagonaleDescendante() {
+        for (int i = 0; i < nbLignes; i++) {
+            matriceCellules[i][i].activerCellule();
+        }
+    }
+
+    public void activerDiagonaleMontante() {
+        for (int i = 0; i < nbLignes; i++) {
+            matriceCellules[i][nbColonnes - i - 1].activerCellule();
+        }
+    }
+
+    // Méthode pour vérifier si toutes les cellules de la grille sont éteintes
+    public boolean cellulesToutesEteintes() {
+        for (int i = 0; i < nbLignes; i++) {
+            for (int j = 0; j < nbColonnes; j++) {
+                if (matriceCellules[i][j].getEtat()) {
+                    return false; // Au moins une cellule est allumée
+                }
+            }
+        }
+        return true; // Toutes les cellules sont éteintes
+    }
+
+    // Redéfinition de la méthode toString pour afficher l'état de la grille
+    @Override
+    public String toString() {
+        StringBuilder grilleString = new StringBuilder();
+
+        // Affichage des indices de colonnes
+        grilleString.append(" |");
+        for (int j = 0; j < nbColonnes; j++) {
+            grilleString.append(" ").append(j).append(" |");
+        }
+        grilleString.append("\n");
+
+        // Ligne de séparation
+        grilleString.append("-+");
+        for (int j = 0; j < nbColonnes; j++) {
+            grilleString.append("---+");
+        }
+        grilleString.append("\n");
+
+        // Affichage de l'état des cellules
+        for (int i = 0; i < nbLignes; i++) {
+            grilleString.append(i).append("|");
+            for (int j = 0; j < nbColonnes; j++) {
+                grilleString.append(" ").append(matriceCellules[i][j]).append(" |");
+            }
+            grilleString.append("\n");
+
+            // Ligne de séparation
+            grilleString.append("-+");
+            for (int j = 0; j < nbColonnes; j++) {
+                grilleString.append("---+");
+            }
+            grilleString.append("\n");
+        }
+
+        return grilleString.toString();
+    }
+
+    // Exemple d'utilisation de la classe GrilleDeJeu
+    public static void main(String[] args) {
+        GrilleDeJeu grille = new GrilleDeJeu(7, 7);
+        grille.melangerMatriceAleatoirement(10);
+        System.out.println(grille);
+        System.out.println("Toutes les cellules sont éteintes : " + grille.cellulesToutesEteintes());
+    }
+}
